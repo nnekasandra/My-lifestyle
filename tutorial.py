2b04deaac0657750b5d6ca96e3cdd47141b7f456
njoku nneka sandra <nnekasandra2016@gmail.com>
2020-03-14 08:17:32 +0100

    first commit

--- tutorial.py
+++ tutorial.py
@@ -4,3 +4,5 @@ registration_no=input('registration_no:\n')
 print(name)
 print(department)
 print(registration_no)
+message=f"my name is {name}, I am in the department of {department} and my registration number is {registration_no}"
+print(message)
